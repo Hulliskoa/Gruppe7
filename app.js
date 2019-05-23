d1811f2c372045ddf2afc1db269e5fe0a8f8f21d
Truls <trulsistre@gmail.com>
2019-05-23 23:21:24 +0200

    Added the start of some backend for github REST API

---
+++ app.js
@@ -0,0 +1,75 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const session = require('express-session');
+const request = require('request');
+const qs = require('querystring');
+const url = require('url');
+const randomString = require('randomstring');
+const bodyParser = require('body-parser');
+
+const csrfString = randomString.generate();
+const port = process.env.PORT || 3000;
+const redirect_uri = process.env.HOST + '/redirect';
+
+
+
+let repositoryName;
+let userName;
+let commitMessage;
+
+let assignemnts;
+
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+/*
+var config = {
+   client_id: process.env.github_client_id,
+   client_secret: process.env.github_client_secret,
+   redirect_url: 'http://localhost:3000/redirect',
+   api_url: 'https://api.github.com', 
+   authorize_url:'https://github.com/login/oauth/authorize?',
+   token_url: 'https://github.com/login/oauth/access_token',
+   user_url: 'https://api.github.com/user',
+   scope: 'user:email'
+ };
+*/
+
+
+// renders the index html/ejs file
+app.get('/', (req, res, next) => {
+        res.render('index', {data: commitMessage});
+});
+
+
+app.post('/inputName', (req, res) => {
+  userName = req.body.user;
+  repositoryName = req.body.repo;
+  let options = {
+  url: 'https://api.github.com/repos/' + userName + '/' + repositoryName + '/commits',
+  method: 'GET',
+  headers: {'Accept': 'application/vnd.github.v3+json', 'User-Agent': 'request'}
+
+  };
+  request(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      let info = JSON.parse(body);
+      commitMessage = info[0].commit.message;
+      console.log(info[0]);
+      res.redirect('/');
+    }else{
+      let info = JSON.parse(body);
+      commitMessage = "Could not find repo. Please try again"
+      res.redirect('/');
+      console.log("error");
+    }
+  });
+});
+
+
+app.listen(port, () => {
+  console.log('Server listening at port ' + port);
+});
+
---
+++ users.js
@@ -0,0 +1,13 @@
+let team = [
+
+    { id: 1, name: "Marge" },
+
+    { id: 2, name: "Lisa" },
+
+    { id: 3, name: "Homer" },
+
+    { id: 4, name: "Maggie" },
+
+    { id: 5, name: "Bart" },
+
+];
