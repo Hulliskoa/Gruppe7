6a8b960c73100f50bd14b45636df70652fb1f022
Truls <trulsistre@gmail.com>
2019-05-29 17:45:26 +0200

    set up links for language docs on mainpage

--- languageDoc.js
+++ languageDoc.js
@@ -1,8 +1,6 @@
-const languageDocs = {
-	Javascript: "https://developer.mozilla.org/bm/docs/Web/JavaScript",
-	Java: "https://docs.oracle.com/javase/7/docs/api/",
-	HTML: "https://devdocs.io/html/",
-	CSS: "https://developer.mozilla.org/en-US/docs/Web/CSS"
+module.exports = {
+	javascript: "https://developer.mozilla.org/bm/docs/Web/JavaScript",
+	java: "https://docs.oracle.com/javase/7/docs/api/",
+	html: "https://developer.mozilla.org/en-US/docs/Web/HTML",
+	css: "https://developer.mozilla.org/en-US/docs/Web/CSS"
 }
-
-module.exports = languageDocs;
