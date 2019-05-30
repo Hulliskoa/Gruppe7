2a4af99224d1a87d4b2dfda8f31d53113eaddb37
Truls <trulsistre@gmail.com>
2019-05-30 22:05:18 +0200

    index.js had lost some code. Recovered it from commit history

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -5,3 +5,32 @@ const animateOctocat = function (){
 	octocat.addClass = "octoAnimation"
 }
 
+
+function post(formID) {
+  // The rest of this code assumes you are not using a library.
+	let form = document.getElementById(formID)
+  form.submit();
+}
+
+function createNewTask()
+{
+	let newItem = document.createElement("div");
+	newItem.classList.add("popup-container");
+	newItem.setAttribute("id", "popup");
+	newItem.innerHTML = '<div class="popup-window"><div class="form-container">'+
+	'<h2>Ny oppgave</h2>'+
+	'<form><div id="group1" class="input-box"><input type="text" required>'+
+	'<span class="highlight"></span><span class="bar"></span>'+
+	'<label>Name</label></div><div id="group2" class="input-box">'+
+	'<input type="text" required><span class="highlight"></span>'+
+	'<span class="bar"></span>'+
+	'<label>Email</label>'+
+	'</div></form></div>'+
+	'<div onclick="exitTask()">Exit new task</div>';
+	document.body.appendChild(newItem);
+}
+
+function exitTask(){
+let popup = document.getElementById("popup");
+	popup.parentNode.removeChild(popup);
+}
