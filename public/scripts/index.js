ecb32e331c3a1f3d3a8cb9fbeda7223a226ba615
Richard Greger Bjørn <richardgreger@techmekk.no>
2019-05-30 14:41:25 +0200

    added index.js again

---
+++ public/scripts/index.js
@@ -0,0 +1,42 @@
+//https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API/Using_IndexedDB
+
+const animateOctocat = function (){
+	let octocat = document.getElementById("octocat");
+	octocat.addClass = "octoAnimation"
+}
+
+
+function post(formID) {
+  // The rest of this code assumes you are not using a library.
+	let form = document.getElementById(formID)
+  form.submit();
+}
+
+function createNewTask()
+{
+	let newItem = document.createElement("div");
+	newItem.classList.add("popup-container");
+	newItem.setAttribute("id", "popup");
+	newItem.innerHTML = '<div class="popup-window"><div class="form-container">'+
+	'<h2>Ny oppgave</h2>'+
+	'<form><div id="group1" class="input-box"><input type="text" required>'+
+	'<span class="highlight"></span><span class="bar"></span>'+
+	'<label>Name</label></div><div id="group2" class="input-box">'+
+	'<input type="text" required><span class="highlight"></span>'+
+	'<span class="bar"></span>'+
+	'<label>Email</label>'+
+	'</div></form></div>'+
+	'<div onclick="exitTask()">Exit new task</div>';
+	document.body.appendChild(newItem);
+}
+
+function exitTask(){
+let popup = document.getElementById("popup");
+<<<<<<< HEAD
+popup.parentNode.removeChild(popup);
+}
+
+=======
+popup.style.visibility = 'hidden';
+}
+>>>>>>> 2a3c57b545e006a79686489252158c672b03152e
