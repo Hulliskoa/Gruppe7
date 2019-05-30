d4531dfc079b7580cd5006c4abead3fb50c49d11
Hessh <haakon.hess@gmail.com>
2019-05-30 14:02:54 +0200

    Made class Task in classes.js

---
+++ classes.js
@@ -0,0 +1,34 @@
+class Task{
+
+    constructor(id, title, owner, category, content){
+        this.id = id;
+        this.title = title;
+        this.owner = owner;
+        this.category = category;
+        this.content = content;
+    }
+
+    getTitle(){
+        return this.title;
+    }
+    getOwner(){
+        return this.owner
+    }
+    getContent(){
+        return this.content;
+    }
+    
+    setTitle(title){
+        this.title = title;
+    }
+    setOwner(owner){
+        this.owner = owner;
+    }
+    setCategory(category){
+        this.category = category;
+    }
+    setContent(content){
+        this.content = content;
+    }
+}
+
