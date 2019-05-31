19e2378d4353611e9aa90b0389616e947fa75aad
Richard Greger Bjørn <richardgreger@techmekk.no>
2019-05-31 11:05:27 +0200

    Added mainpage.js

---
+++ public/scripts/mainpage.js
@@ -0,0 +1,37 @@
+
+
+function post(formID) {
+    // The rest of this code assumes you are not using a library.
+      let form = document.getElementById(formID)
+    form.submit();
+  }
+  
+  function createNewTask()
+  {
+      let newItem = document.createElement("div");
+      newItem.classList.add("popup-container");
+      newItem.setAttribute("id", "popup");
+      newItem.innerHTML = '<div class="popup-window"><div class="form-container">'+
+      '<h2>Ny oppgave</h2>'+
+      '<form><div id="group1" class="input-box"><input type="text" required>'+
+      '<span class="highlight"></span><span class="bar"></span>'+
+      '<label>Name</label></div><div id="group2" class="input-box">'+
+      '<input type="text" required><span class="highlight"></span>'+
+      '<span class="bar"></span>'+
+      '<label>Email</label>'+
+      '</div></form></div>'+
+      '<div onclick="exitTask()">Exit new task</div>';
+      document.body.appendChild(newItem);
+  }
+  
+  function exitTask(){
+  let popup = document.getElementById("popup");
+  popup.parentNode.removeChild(popup);
+  }
+  
+  
+  // Anonymous function. Always provoked 
+  (function (){
+    
+        createNewTask();    
+  })();
