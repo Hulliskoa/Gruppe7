c78abd9bd29beeac2417ce2fb4ff5cbf5178813a
Richard Greger Bjørn <richardgreger@techmekk.no>
2019-06-05 14:53:19 +0200

    Test

--- public/scripts/mainpage.js
+++ public/scripts/mainpage.js
@@ -1,4 +1,3 @@
-
 let main = document.getElementsByTagName("main");
 
 // function for the select all checkbox on mainpage
@@ -282,3 +281,62 @@ function confirmDeletion(task){
             '</div>';
       document.body.appendChild(newItem);
 };
+
+// Drag and drop
+
+let _doc = window.document;
+let selectedElement,
+  selectedTarget;
+
+
+function dragStart(event)
+{
+    //event.preventDefault();
+    selectedElement = event.currentTarget;
+    
+}
+
+function dragEnd() {
+
+  }
+
+function dragOver()
+{
+    event.preventDefault();
+    event.dataTransfer.dropEffect = "copy";
+}
+
+function dragEnter()
+{
+    event.preventDefault();
+    event.currentTarget.appendChild(selectedElement);
+}
+
+function dragLeave()
+{
+}
+
+function dragDrop()
+{
+    selectedTarget = event.currentTarget;
+    
+    selectedTarget.appendChild(selectedElement);
+
+    /*  Hvis vi vil styre hvor elementene havner
+        
+    if(selectedTarget == column1)
+    {
+        // Høyere [x] på childNotes = undefined
+    selectedTarget.insertBefore(selectedElement, column1.childNodes[4]);
+    }
+    else if(selectedTarget == column2)
+    {
+        selectedTarget.insertBefore(selectedElement, column2.childNodes[4]);
+    }
+    else
+    {
+        selectedTarget.insertBefore(selectedElement, column3.childNodes[4]);
+        console.log(column3.childNodes.length);
+    }
+    */
+}
